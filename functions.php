f24c3a8302d4c08ef1bbcfa29f0b9617f9fb8f0c
Damian Cendan <59464234+DamianCendan@users.noreply.github.com>
2022-08-19 01:06:25 -0700

    Section 6 Templates
    65. Creating a Header and Footer Template Part

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // Hooks
 //-Adding the first hook to equeue the style files
 add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action('wp_head', 'u_head', 5);
